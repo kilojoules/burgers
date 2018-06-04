7440dc048735a61ddaab2f12499e98be1b05f68b
Julian Quick <jaq8@humboldt.edu>
2018-06-04 12:37:44 -0600

    Added D analysis. Not enough data for cv2 so I'm turning it off for now.

--- fullAnalysis.py
+++ fullAnalysis.py
@@ -20,12 +20,21 @@ from sklearn.utils import shuffle
 myhfdat = pd.read_csv('./collected.csv')
 #d3 = pd.read_csv('./timedsamples.dat')
 #dat = pd.concat([d1, d2, d3])
-myhfdat.rename(columns={'nx7000':'hf', 'nx400':'lf2', 'nx7000_red':'lf1', 'nu':'visc', 'tnx7000':'thf', 'tnx400': 'tlf2'}, inplace=1)
+myhfdat.rename(columns={'nx7000':'hf', 'nx400':'lf2', 'nx7000_red':'lf1', 'tnx7000_red':'tlf1', 'nu':'visc', 'tnx7000':'thf', 'tnx400': 'tlf2'}, inplace=1)
 myhfdat = shuffle(myhfdat)
 TRUTH = np.mean(myhfdat.hf)
 mylfdat = pd.read_csv('./lfcollected.csv')
 mylfdat.rename(columns={'nx7000':'hf', 'nx7000_red':'lf1', 'nx400':'lf2', 'nu':'visc', 'tnx7000':'thf', 'tnx7000_red': 'tlf1', 'tnx400': 'tlf2'}, inplace=1)
 #LF2M = np.mean(lfdat.lf2)
+omega_1 = np.mean(myhfdat.thf)
+omega_2 = np.mean(mylfdat.tlf1)
+omega_3 = np.mean(mylfdat.tlf2)
+rho_1_2 = np.corrcoef(myhfdat.hf, myhfdat.lf1)[0][1]
+rho_1_3 = np.corrcoef(myhfdat.hf, myhfdat.lf2)[0][1]
+
+r_cv = np.sqrt(omega_1 * rho_1_3**2 / (omega_3 * (1 - rho_1_3**2)))
+r_cv2_1 = np.sqrt(omega_1 * (rho_1_2**2 - rho_1_3**2) / (omega_2 * (1 - rho_1_2**2)))
+r_cv2_2 = np.sqrt(omega_1 * rho_1_3**2 / (omega_3 * (1 - rho_1_2**2)))
 mylfdat = shuffle(mylfdat)
 def cv(x, LF, hfdat, lfdat):
    #alpha = np.corrcoef(hfdat.hf, hfdat.lf2)[0][1] * np.sqrt(np.var(hfdat.hf) / np.var(hfdat.lf2))
@@ -35,6 +44,9 @@ def cv(x, LF, hfdat, lfdat):
    cv = np.mean(hfdat.hf[:x]) + alpha * (np.mean(lowfis.lf2[:LF]) - np.mean(hfdat.lf2[:x]))
    return cv
 cvv = np.vectorize(cv, excluded=[2,3])
+def cvtime(x, LF, hfdat, lfdat):
+    return(np.sum([hfdat.thf[:x].sum(),  hfdat.tlf2[:x].sum(), lfdat.tlf2.values[:LF].sum()]))
+cvtimev = np.vectorize(cvtime, excluded=[2, 3])
 
 def cv2(x, LF1, LF2, hfdat, lfdat):
    lowfis = pd.concat([hfdat, lfdat])
@@ -44,37 +56,82 @@ def cv2(x, LF1, LF2, hfdat, lfdat):
    return cv
 cv2v = np.vectorize(cv2, excluded=[3,4])
 
-def lf2(N_LF2, lfdat): return np.mean(lfdat.lf2[:N_LF2])
-lf2v = np.vectorize(lf2, excluded=[1])
+def cv2time(x, LF1, LF2, hfdat, lfdat):
+   return np.sum([hfdat.thf[:x].sum(), hfdat.tlf1[:x].sum(), hfdat.tlf2[:x].sum(), lfdat.tlf1[:LF1].sum(), lfdat.tlf2[:LF2].sum()])
+cv2timev = np.vectorize(cv2time, excluded=[3, 4])
+
+def lf(LF, lfdat): return np.mean(lfdat.lf1[:LF])
+lfv = np.vectorize(lf, excluded=[1])
+
+def lftime(LF, lfdat): return np.sum(lfdat.tlf1[:LF])
+lftimev = np.vectorize(lftime, excluded=[1])
 
 def hf(N_HF, hfdat): return np.mean(hfdat.hf[:N_HF])
 hfv = np.vectorize(hf, excluded=[1])
 
-x = np.arange(2, 10)
+def hftime(HF, hfdat): return np.sum(hfdat.thf[:HF])
+hftimev = np.vectorize(hftime, excluded=[1])
+
+x = np.arange(2, 100)
 #plt.plot(x, (TRUTH - cv2v(x, x * 910, x * 5700, dat, lfdat))**2, label='CV')
 #plt.plot(x, (TRUTH - lf2v(x, lfdat))**2, label='LF')
-LFMAX = 100
+LFMAX = max([r_cv, r_cv2_1, r_cv2_2]) / x[-1]
 dats = np.array_split(myhfdat, myhfdat.shape[0] / x[-1])
 lfdats = np.array_split(mylfdat, mylfdat.shape[0] / LFMAX / x[-1])
 BS = min(len(dats), len(lfdats))
 #BS = 20
 hfest = np.zeros((x.size, BS))
+lfest = np.zeros((x.size, BS))
 cvest = np.zeros((x.size, BS))
 cv2est = np.zeros((x.size, BS))
+hft = np.zeros((x.size, BS))
+lft = np.zeros((x.size, BS))
+cvt = np.zeros((x.size, BS))
+cv2t = np.zeros((x.size, BS))
 for _ in range(BS):
    print(_,'/',BS)
    dat = dats[_]
+   dat.reset_index(inplace=True)
    lfdat = lfdats[_]
+   lfdat.reset_index(inplace=True)
    hfest[:, _] = hfv(x, dat)
-   cvest[:, _] = cvv(x, LFMAX * x, dat, lfdat)
-   cv2est[:, _] = cv2v(x, LFMAX * x, LFMAX * x, dat, lfdat)
+   lfest[:, _] = lfv(x, lfdat)
+   cvest[:, _] = cvv(x, (r_cv * x).astype(int), dat, lfdat)
+   cv2est[:, _] = cv2v(x, (r_cv2_1 * x).astype(int), x * LFMAX.astype(int) * x, dat, lfdat)
+   hft[:, _] = hftimev(x, dat)
+   lft[:, _] = lftimev(x, lfdat)
+   cvt[:, _] = cvtimev(x, (r_cv * x).astype(int), dat, lfdat)
+   cv2t[:, _] = cv2timev(x, (r_cv2_1 * x).astype(int), x * LFMAX.astype(int) * x, dat, lfdat)
+
 #plt.plot(x, (TRUTH - np.mean(hfest, 1))**2, label='HF', marker='x')
 #plt.plot(x, (TRUTH - np.mean(cvest, 1))**2, label='CV', marker='x')
 plt.plot(x, np.mean(np.abs(TRUTH - hfest), 1), label='HF', marker='x')
+plt.plot(x, np.mean(np.abs(TRUTH - lfest), 1), label='LF', marker='x')
 plt.plot(x, np.mean(np.abs(TRUTH - cvest), 1), label='CV', marker='x')
 plt.plot(x, np.mean(np.abs(TRUTH - cv2est), 1), label='CV2', marker='x')
 plt.legend()
 plt.yscale('log')
 plt.xscale('log')
-plt.savefig('graphed.pdf')
-plt.show()
+plt.savefig('convPlot.pdf')
+plt.clf()
+
+plt.plot(hft.mean(1), np.mean(np.abs(TRUTH - hfest), 1), label='HF', marker='x')
+plt.plot(lft.mean(1), np.mean(np.abs(TRUTH - lfest), 1), label='LF', marker='x')
+plt.plot(cvt.mean(1), np.mean(np.abs(TRUTH - cvest), 1), label='CV', marker='x')
+plt.plot(cv2t.mean(1), np.mean(np.abs(TRUTH - cv2est), 1), label='CV2', marker='x')
+plt.yscale('log')
+plt.xscale('log')
+plt.legend()
+plt.savefig('timePlot.pdf')
+plt.clf()
+
+plt.plot(x * omega_1, np.mean(np.abs(TRUTH - hfest), 1), label='HF', marker='x')
+plt.plot(x * omega_2, np.mean(np.abs(TRUTH - lfest), 1), label='LF', marker='x')
+plt.plot(x * omega_1 + r_cv * x * omega_2, np.mean(np.abs(TRUTH - cvest), 1), label='CV', marker='x')
+plt.plot(x * omega_1 + r_cv2_1 * x * omega_2 + r_cv2_2 * omega_3, np.mean(np.abs(TRUTH - cv2est), 1), label='CV2', marker='x')
+plt.legend()
+plt.yscale('log')
+plt.xscale('log')
+plt.savefig('realtimePlot.pdf')
+plt.clf()
+
